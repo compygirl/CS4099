2522583b86f3b3bcd7ca304a427cc2064d99f2cd
Aigerim Yessenbayeva <ayessenbayeva89@gmail.com>
2016-03-25 16:05:50 +0600

    no message

---
+++ MapBackgroud/MapBackground/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  MapBackgroud
+//
+//  Created by Aigerim Yessenbayeva on 02/02/2014.
+//  Copyright (c) 2014 Aigerim Yessenbayeva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
